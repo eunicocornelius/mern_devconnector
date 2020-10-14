7b8309e57e8d296b26cfb19905a9372a4d329e14
Nico <eunico.cornelius@gmail.com>
2020-10-14 23:48:32 +0700

    added routes and connection to mongodb atlas

---
+++ routes/api/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// @route           GET api/auth
+// @description     Test route
+// @access          Public
+router.get('/', (req, res) => res.send('Auth route'));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+// Main server
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+
 app.get('/', (req,res) => res.send('API running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
