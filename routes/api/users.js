98fec9a2edfa9532eed7ba2b7c40e98a16c89e2c
Nico <eunico.cornelius@gmail.com>
2020-10-15 00:04:38 +0700

    added user post validation with express validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator')
 
-// @route           GET api/users
-// @description     Test route
+// @route           POST api/users
+// @description     Register user
 // @access          Public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', [
+    // Express Validator: To provide feedback API so that we can display the error message on the frontend
+    check('name','Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+],(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array()});
+    }
+
+    res.send('User route')
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({extended: false}));
 
 app.get('/', (req,res) => res.send('API running'));
 
