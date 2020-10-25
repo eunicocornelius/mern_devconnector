38d055d164b7d8066797273fe2f2d895354df2a9
Nico <eunico.cornelius@gmail.com>
2020-10-25 15:54:03 +0700

    commit for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Main server
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -8,15 +9,26 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
-app.get('/', (req,res) => res.send('API running'));
+// For Dev Build
+// app.get('/', (req,res) => res.send('API running'));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
